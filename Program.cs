19f0ad1cfe0ed0cfde97be9665e5c3c89c3d4fe4
Mohannad Zeido <mohannadzeido@Mohannads-MacBook-Pro.local>
2020-04-17 16:09:32 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TL_Data_API_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
